bb3a0eee1d52088bf36e2fc02e83e25c24bb4ab8
Svenan <egorc42@gmail.com>
2021-05-04 19:50:58 +0500

    Moved project to MVC, added basic models.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        DBContext db = new DBContext();
+        public ActionResult Index()
+        {
+            IEnumerable<User> users = db.Users;
+            ViewBag.Users = users;
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class User
+    {
+        [Key]
+        public int UsrId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
