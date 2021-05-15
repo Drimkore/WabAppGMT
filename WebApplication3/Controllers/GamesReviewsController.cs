5720a597c24e196638063b8639391bf50e4ed696
Drimkore <art.2000m.ab@gmail.com>
2021-05-15 17:34:02 +0500

    Начата работа над страницей отзывов

--- WebApplication3/Controllers/GamesReviewsController.cs
+++ WebApplication3/Controllers/GamesReviewsController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
+using WebApplication3.Models;
+using System.Web.Security;
+using System.Net.Mail;
+using System.Net;
 
 namespace WebApplication3.Controllers
 {
     public class GamesReviewsController : Controller
     {
+        DBContext games = new DBContext();
         // GET: GamesReviews
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult GamesReviews()
         {
+            IEnumerable<Game> games1 = games.Games;
+            ViewBag.Games = games1;
             return View();
         }
     }
