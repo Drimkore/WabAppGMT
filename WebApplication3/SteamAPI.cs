bc53895a3818d37a7cd8fdb9499278395f99cd52
NotRude <69007331+NotRude@users.noreply.github.com>
2021-05-21 12:06:56 +0500

    Update SteamAPI.cs
    Добавить HtmlAgilityPack в проект

--- WebApplication3/SteamAPI.cs
+++ WebApplication3/SteamAPI.cs
@@ -1,10 +1,12 @@
-﻿using System;
+using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Web;
+using System.Xml;
 
-namespace WebApplication3
+namespace testapp2
 {
     public class UsersGamesInfo
     {
@@ -19,7 +21,7 @@ namespace WebApplication3
         {
             return GamesCount;
         }
-        public Dictionary<string,string> GetGamesIDnTime()
+        public Dictionary<string, string> GetGamesIDnTime()
         {
             return GamesIDnTime;
         }
@@ -58,28 +60,59 @@ namespace WebApplication3
         }
         public static GameInfo GetGameInfo(string gameID)
         {
-            var wc = new WebClient();
-            var answer = wc.DownloadString("https://store.steampowered.com/app/"+gameID+"/");
-            var name = GetInfo(answer, "apphub_AppName\">", "</div>");
-            if (name.Length > 100)
-                name = gameID;
-            return new GameInfo(name, GetInfo(answer, "img class=\"game_header_image_full\" src=\"", "\">"),
-                GetInfo(answer, "<div class=\"game_description_snippet\">", "</div>"));
+            HtmlWeb web = new HtmlWeb() {UseCookies = true };
+            var htmlDoc = web.Load("https://store.steampowered.com/app/" + gameID + "/?l=russian");
+
+            var node = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='apphub_AppName']");
+            if(node == null)
+            {
+                return new GameInfo("NTE", "", "");
+            }
+            var name = node.InnerText.Trim();
+            node = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='game_description_snippet']");
+            var descr = node.InnerText.Trim();
+            node = htmlDoc.DocumentNode.SelectSingleNode("//img[@class='game_header_image_full']");
+            var img = node.Attributes["src"].Value.Trim();
+            return new GameInfo(name, img, descr);
         }
         public static UsersGamesInfo GetUsersGamesInfo(string userID)
         {
-            var wc = new WebClient();
-            var answer = wc.DownloadString("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=61115E80A2CDB0D150ACC0D51E2FCF4F&steamid=" + userID + "&format=xml");
-            var text = answer.Remove(0, answer.IndexOf("<game_count>") + "<game_count>".Length);
-            var count = text.Remove(text.IndexOf("</"));
             var gameStats = new Dictionary<string, string>();
-            while (text.IndexOf("<appid>") != -1)
+            var count = "";
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=61115E80A2CDB0D150ACC0D51E2FCF4F&steamid=76561198091848209&format=xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            foreach (XmlNode xnode in xRoot)
             {
-                text = text.Remove(0, text.IndexOf("<appid>") + "<appid>".Length);
-                var gameID = text.Remove(text.IndexOf("</appid>"));
-                text = text.Remove(0, text.IndexOf("<playtime_forever>") + "<playtime_forever>".Length);
-                var playTime = text.Remove(text.IndexOf("</playtime_forever>"));
-                gameStats.Add(gameID, playTime);
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    if (childnode.Name == "#text")
+                    {
+                        count = childnode.InnerText;
+                    }
+                    if (childnode.Name == "message")
+                    {
+                        var id = "";
+                        var time = "";
+                        foreach (XmlNode childnode2 in childnode.ChildNodes)
+                        { 
+                            if (childnode2.Name == "appid")
+                            {
+                                id = childnode2.InnerText;
+                            }
+                            if (childnode2.Name == "playtime_forever")
+                            {
+                                time = childnode2.InnerText;
+                            }
+                            if (id != "" && time != "")
+                            {
+                                gameStats.Add(id, time);
+                                id = "";
+                                time = "";
+                            }
+                        }
+                    }
+                }
             }
             return new UsersGamesInfo(count, gameStats);
         }
